5e6c751a6931ba4f07ffe2aab91176b6bbbfcf1e
eliserothberg <rothberg.elise@gmail.com>
2016-10-13 02:27:19 -0700

    heroku and JAWSDB add

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
 var mysql = require('mysql');
 var connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-  } else {
-    connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'password',
-    database : 'items'
-  });
-};
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+//   } else {
+//     connection = mysql.createConnection({
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : 'password',
+//     database : 'items'
+//   });
+// };
 
 connection.connect(function(err) {
   if (err) {
